5504845fd77a0d269b0757ec83fc4b790a15669c
Zerdiron <sanchezdamien34@hotmail.fr>
2017-01-18 15:59:33 +0100

    Ajoute de boucle

--- Plus_Ou_Moins/Program.cs
+++ Plus_Ou_Moins/Program.cs
@@ -8,11 +8,21 @@ namespace Plus_Ou_Moins
 {
 	class Program
 	{
+		static readonly Random numberRandomizer = new Random();
 		static void Main(string[] args)
 		{
-			int numberRandomized=5, numberUser; // Initialisation.
-			string userInput= Console.ReadLine(); // Input de l'utilsiateur.
-			bool Conversion = int.TryParse(userInput, out numberUser); // Conversion de l'input de l'utilisateur en int.
+			int numberRandomized=5, numberUser, miniRandom=0, maxiRandom=100; bool Conversion = false; // Initialisation.
+			Console.WriteLine("Entrez un nombre entre {0} et {1}", miniRandom, maxiRandom);
+			do
+			{
+				string userInput = Console.ReadLine(); // Input de l'utilsiateur.
+				Conversion = int.TryParse(userInput, out numberUser); // Conversion de l'input de l'utilisateur en int.
+				if (!Conversion)
+				{
+					Console.WriteLine("Entrez un nombre valide !");
+				}
+			} while (!Conversion);
+			
 			if (numberRandomized < numberUser)
 			{
 				Console.WriteLine("Trop grand !");
