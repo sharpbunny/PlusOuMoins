163f3ebb7810edd582c815526050b2d6b8e92e83
jringenbach <34011-14-08@exchange.ad.afpanet>
2017-01-18 16:30:45 +0100

    indentation

--- Plus_Ou_Moins/Program.cs
+++ Plus_Ou_Moins/Program.cs
@@ -6,50 +6,50 @@ using System.Threading.Tasks;
 
 namespace Plus_Ou_Moins
 {
-    class Program
-    {
-        static readonly Random numberRandomizer = new Random();
-        static void Main(string[] args)
-        {
-            // DECLARATION DES VARIABLES
-            int numberRandomized, miniRandom = 0, maxiRandom = 100, numberUser = miniRandom - 1; bool Conversion = false; // Initialisation.
+	class Program
+	{
+		static readonly Random numberRandomizer = new Random();
+		static void Main(string[] args)
+		{
+			// DECLARATION DES VARIABLES
+			int numberRandomized, miniRandom = 0, maxiRandom = 100, numberUser = miniRandom - 1; bool Conversion = false; // Initialisation.
 
-		    numberRandomized = numberRandomizer.Next(miniRandom, maxiRandom); // Génération du nombre.
+			numberRandomized = numberRandomizer.Next(miniRandom, maxiRandom); // Génération du nombre.
 
-            Console.WriteLine("Entrez un nombre entre {0} et {1}", miniRandom, maxiRandom);
+			Console.WriteLine("Entrez un nombre entre {0} et {1}", miniRandom, maxiRandom);
 
-            // BOUCLE DU JEU
+			// BOUCLE DU JEU
 			while (numberUser != numberRandomized)
 			{
-			  // Cette boucle va nous permettre de tester si le joueur entre un nombre valide.
-			  do
-			  {
-				string userInput = Console.ReadLine(); // Input de l'utilisateur.
-				Conversion = int.TryParse(userInput, out numberUser); // Conversion de l'input de l'utilisateur en int.
-				if (!Conversion)
+				// Cette boucle va nous permettre de tester si le joueur entre un nombre valide.
+				do
 				{
-				  Console.WriteLine("Entrez un nombre valide !");
+					string userInput = Console.ReadLine(); // Input de l'utilisateur.
+					Conversion = int.TryParse(userInput, out numberUser); // Conversion de l'input de l'utilisateur en int.
+					if (!Conversion)
+					{
+						Console.WriteLine("Entrez un nombre valide !");
+					}
+				} while (!Conversion && (numberUser >= miniRandom && numberUser <= maxiRandom));
+
+				// Si le nombre du joueur est supérieur au nombre aléatoire
+				if (numberRandomized < numberUser)
+				{
+					Console.WriteLine("Trop grand !");
 				}
-			  } while (!Conversion && (numberUser >= miniRandom && numberUser <= maxiRandom));
 
-			  // Si le nombre du joueur est supérieur au nombre aléatoire
-			  if (numberRandomized < numberUser)
-			  {
-				Console.WriteLine("Trop grand !");
-			  }
+				// Si le nombre du joueur est inférieur au nombre aléatoire
+				else if (numberRandomized > numberUser)
+				{
+					Console.WriteLine("Trop petit !");
+				}
 
-			  // Si le nombre du joueur est inférieur au nombre aléatoire
-			  else if (numberRandomized > numberUser)
-			  {
-				Console.WriteLine("Trop petit !");
-			  }
-
-			  // Si le nombre du joueur est égal au nombre aléatoire
-			  else if (numberRandomized == numberUser)
-			  {
-				Console.WriteLine("Félicitation, vous avez trouvé le nombre !");
-			  }
+				// Si le nombre du joueur est égal au nombre aléatoire
+				else if (numberRandomized == numberUser)
+				{
+					Console.WriteLine("Félicitation, vous avez trouvé le nombre !");
+				}
 			}
-        }
-    }
+		}
+	}
 }
