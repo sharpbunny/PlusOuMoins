f6bf763c56b472e81bc939d93ede8cbed36ee8c1
Zerdiron <sanchezdamien34@hotmail.fr>
2017-01-18 15:47:53 +0100

    Initialisation du projet avec une base de code

--- Plus_Ou_Moins/Program.cs
+++ Plus_Ou_Moins/Program.cs
@@ -10,6 +10,21 @@ namespace Plus_Ou_Moins
 	{
 		static void Main(string[] args)
 		{
+			int numberRandomized=5, numberUser; // Initialisation.
+			string userInput= Console.ReadLine(); // Input de l'utilsiateur.
+			bool Conversion = int.TryParse(userInput, out numberUser); // Conversion de l'input de l'utilisateur en int.
+			if (numberRandomized < numberUser)
+			{
+				Console.WriteLine("Trop grand !");
+			}
+			else if (numberRandomized > numberUser)
+			{
+				Console.WriteLine("Trop petit !");
+			}
+			else if (numberRandomized == numberUser)
+			{
+				Console.WriteLine("Félicitation vous avez trouvé le nombre !");
+			}
 		}
 	}
 }
