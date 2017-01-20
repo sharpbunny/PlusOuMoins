a8a02884e7d58f850c0908e2ea53941502493c48
jringenbach <34011-14-08@exchange.ad.afpanet>
2017-01-20 09:42:18 +0100

    Ajout des fichiers du windows form au projet

---
+++ Plus_Ou_Moins/wfPlusOuMoins.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Plus_Ou_Moins
+{
+	public partial class wfPlusOuMoins : Form
+	{
+		public wfPlusOuMoins()
+		{
+			InitializeComponent();
+		}
+
+		private void wfPlusOuMoins_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
